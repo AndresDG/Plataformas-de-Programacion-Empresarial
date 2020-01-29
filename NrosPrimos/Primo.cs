9047ab8c3bb2280fcf59f347e2724bde9d989a89
Andrés Díaz G <33076595+AndresDG@users.noreply.github.com>
2020-01-29 11:00:04 -0500

    Add files via upload

---
+++ NrosPrimos/Primo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NrosPrimos
+{
+    class Primo
+    {
+
+        private int primo;
+
+        public Primo() {
+            primo = 1;
+        }
+
+        public Primo(int Primo)
+        {
+            this.primo = Primo;
+        }
+
+        public int GPrimo { get => primo; set => primo = value; }
+
+        /*private bool EsPrimo(int num) {
+            num = num + 1;
+            int cont = num - 1;
+            bool verdad = false;
+
+            while (cont >= 1)
+            {
+                if (num % cont == 0) {
+
+                    break;
+
+                }
+
+                cont = cont - 1;
+
+                
+                
+            }
+            if (verdad) return true;
+
+            return false;
+        }*/
+        public int NroPrimo(int pos){
+
+            //int cont = 1;
+            int contador = 0;
+
+            while (contador < pos) {
+                primo = this.primo + 1;
+                int cont = this.primo - 1;
+                //EsPrimo(this.primo);
+                while (cont >= 1)
+                {
+                    if (this.primo % cont == 0)
+                    {
+
+                        break;
+
+                    }
+
+                    cont = cont - 1;
+                }
+
+                if (cont <= 1)
+                {
+                    contador = contador + 1;
+
+                }
+            }
+
+
+            return this.primo;
+        }
+
+
+
+    }
+
+
+   
+}
---
+++ NrosPrimos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NrosPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primo primo = new Primo();
+            Console.WriteLine("Ingrese la posicion del numero primo que desea conocer");
+            int posi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n" + "El numero primo en la posicion " + posi +" es: ");
+
+            Console.WriteLine(primo.NroPrimo(posi));
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
